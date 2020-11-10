a91a5eea4029d82f37d4f4c801d59abb9b0ce5f7
DarkangelKid1109 <darkangelkid1109@gmail.com>
2020-11-10 11:45:46 +0700

    update

--- src/api/controllers/datasync.controller.js
+++ src/api/controllers/datasync.controller.js
@@ -111,56 +111,6 @@ exports.HandleCybersecurity = async (req, res, next) => {
   try {
     const items = await Datasync.findAll({ where: { appType: 'Cybersecurity' } });
 
-    // Promise.all()
-    /* await Promise.all(
-      items.map(async (item) => {
-        let data = JSON.parse(item.data);
-        //console.log(data.ctx.payload.filtered);
-        await Promise.all(
-          data.ctx.payload.filtered.map(async (i) => {
-            let { _source } = i;
-            let itemANM = Object.assign(
-              {},
-              {
-                appType: 'Cybersecurity',
-                data: item.data,
-                fidelity: _source.fidelity,
-                srcip: _source.srcip,
-                srcip_geo_countryCode: _source.srcip_geo.countryCode,
-                srcip_geo_countryName: _source.srcip_geo.countryName,
-                srcip_geo_city: _source.srcip_geo.city,
-                srcip_geo_latitude: _source.srcip_geo.latitude,
-                srcip_geo_longitude: _source.srcip_geo.longitude,
-                dstip: _source.dstip,
-                dstip_geo_countryCode: _source.srcip_geo.countryCode,
-                dstip_geo_countryName: _source.srcip_geo.countryName,
-                dstip_geo_city: _source.srcip_geo.city,
-                dstip_geo_latitude: _source.srcip_geo.latitude,
-                dstip_geo_longitude: _source.srcip_geo.longitude,
-                event_name: _source.event_name,
-                event_category: _source.event_category,
-                event_score: _source.event_score,
-                alert_time: _source.alert_time,
-                timestamp: _source.timestamp,
-                timestamp_utc: _source.timestamp_utc,
-                time: moment.utc(_source.timestamp_utc).format('YYYY-MM-DD HH:mm:ss'),
-              },
-            );
-            //2020-10-30 08:06:18.5950000 +00:00
-            try {
-              await ANMsync.create(itemANM);
-            } catch (err) {
-              console.log('LOI');
-              // console.log(err);
-            }
-          }),
-        );
-        item.check = 4;
-        await item.save();
-      }),
-    ); */
-
-    //  res.status(httpStatus.CREATED);
     return res.json(items.length);
   } catch (error) {
     next(error);
@@ -170,38 +120,6 @@ exports.HandleCybersecurity = async (req, res, next) => {
 exports.HandleCamera = async (req, res, next) => {
   try {
     const items = await Datasync.findAll({ where: { appType: 'Camera' } });
-
-    // Promise.all()
-    await Promise.all(
-      items.map(async (item) => {
-        let data = JSON.parse(item.data);
-
-        let itemANM = Object.assign(
-          {},
-          {
-            appType: 'Camera',
-            data: item.data,
-            title: data.notification.body,
-            time_txt: data.data.time,
-            time: moment.utc(data.data.time).format('YYYY-MM-DD HH:mm:ss'),
-            violate_type: data.data.violate_type,
-            camera_location: data.data.camera.location,
-            camera_location_latitude: data.data.camera.lonlat.latitude,
-            camera_location_longitude: data.data.camera.lonlat.longitude,
-          },
-        );
-        //2020-10-30 08:06:18.5950000 +00:00
-        try {
-          await CAMERAsync.create(itemANM);
-        } catch (err) {
-          console.log('LOI');
-          // console.log(err);
-        }
-
-        item.check = 4;
-        await item.save();
-      }),
-    );
 
     //  res.status(httpStatus.CREATED);
     return res.json(items.length);
